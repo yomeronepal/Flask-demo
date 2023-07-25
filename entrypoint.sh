292be389782a8a4c5ffea4ab7a3afbe819eef401
Rojesh Prajapati <yomeronepal7@gmail.com>
2023-07-25 19:25:57 +0545

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
+# if [ "$DATABASE" = "postgres" ]
+# then
+#     echo "Waiting for postgres..."
 
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
+#     while ! nc -z $SQL_HOST $SQL_PORT; do
+#       sleep 0.1
+#     done
 
-    echo "PostgreSQL started"
-fi
+#     echo "PostgreSQL started"
+# fi
 
 
-exec "$@"
+# exec "$@"
+
+# activate the virtual environment
+. /opt/pysetup/.venv/bin/activate
+
+flask db upgrade
+
+python app.py
